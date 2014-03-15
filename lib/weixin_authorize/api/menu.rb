6768d300815944876349bccbe2df1fd64340b2be
lanrion <huaitao-deng@foxmail.com>
2014-03-16 02:30:19 +0800

    refactor menu.rb with "http_get" and "http_post" method

--- lib/weixin_authorize/api/menu.rb
+++ lib/weixin_authorize/api/menu.rb
@@ -6,28 +6,28 @@ module WeixinAuthorize
       # 自定义菜单查询接口
       # https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN
       def menu
-        get_menu_url = "#{menu_base_url}/get?#{access_token_param}"
-        JSON.parse(RestClient.get(get_menu_url))
+        get_menu_url = "#{menu_base_url}/get"
+        http_get(get_menu_url)
       end
 
       # 自定义菜单删除接口
       # https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN
       def delete_menu
-        delete_menu_url = "#{menu_base_url}/delete?#{access_token_param}"
-        JSON.parse(RestClient.get(delete_menu_url))
+        delete_menu_url = "#{menu_base_url}/delete"
+        http_get(delete_menu_url)
       end
 
       # 自定义菜单创建接口
       # https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN
       def create_menu(menu)
-        create_menu_url = "#{menu_base_url}/create?#{access_token_param}"
-        JSON.parse(RestClient.post(create_menu_url, menu))
+        create_menu_url = "#{menu_base_url}/create"
+        http_post(create_menu_url, menu)
       end
 
       private
 
         def menu_base_url
-          "#{endpoint}/menu"
+          "/menu"
         end
 
     end
--- spec/api/menu_spec.rb
+++ spec/api/menu_spec.rb
@@ -6,7 +6,9 @@ describe WeixinAuthorize::Api::Menu do
   it "can create a menu" do
     menu = '{"button":[{"type":"click","name":"今日歌曲","key":"V1001_TODAY_MUSIC"},{"type":"click","name":"歌手简介","key":"V1001_TODAY_SINGER"},{"name":"菜单","sub_button":[{"type":"view","name":"搜索","url":"http://www.soso.com/"},{"type":"view","name":"视频","url":"http://v.qq.com/"},{"type":"click","name":"赞一下我们","key":"V1001_GOOD"}]}]}'
     response = $client.create_menu(menu)
+    puts response
     expect(response["errcode"]).to eq(0)
+
   end
 
   it "can get a weixin Menu info" do
