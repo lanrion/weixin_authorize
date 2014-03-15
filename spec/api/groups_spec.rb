5bd539eaa096ed3081ab733a864cd8e3fd20a4fc
lanrion <huaitao-deng@foxmail.com>
2014-03-16 02:30:32 +0800

    refactor groups.rb with "http_get" and "http_post" method

--- spec/api/groups_spec.rb
+++ spec/api/groups_spec.rb
@@ -2,6 +2,11 @@ require "spec_helper"
 describe WeixinAuthorize::Api::Groups do
   it "create a group" do
     response = $client.create_group("test")
-    response
+    puts response
+  end
+
+  it "get groups" do
+    groups = $client.groups
+    puts groups
   end
 end
