384386140846ec1069cf3ee63ad8dee736327877
lanrion <huaitao-deng@foxmail.com>
2014-03-19 15:24:19 +0800

    updated user_spec.rb

--- spec/api/user_spec.rb
+++ spec/api/user_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe WeixinAuthorize::Api::User do
   it "can get a weixin User info" do
     user_info = $client.user(ENV["OPENID"])
-    puts user_info
+    expect(user_info["openid"]).to eq(ENV["OPENID"])
   end
 
   it "can get followers infos" do
     followers = $client.followers
-    puts followers
+    expect(followers.keys).to eq(["total", "count", "data", "next_openid"])
   end
 end
