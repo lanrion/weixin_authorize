54121afbf9760fcfa6a5f6d9ce991e67d64976ba
lanrion <huaitao-deng@foxmail.com>
2014-03-30 22:15:54 +0800

    refactor with api.rb

--- lib/weixin_authorize.rb
+++ lib/weixin_authorize.rb
@@ -1,12 +1,7 @@
 require "rest-client"
 require "multi_json"
 require "weixin_authorize/config"
-require "weixin_authorize/api/user"
-require "weixin_authorize/api/menu"
-require "weixin_authorize/api/custom"
-require "weixin_authorize/api/groups"
-require "weixin_authorize/api/qrcode"
-require "weixin_authorize/api/media"
+require "weixin_authorize/api"
 require "weixin_authorize/client"
 
 module WeixinAuthorize
