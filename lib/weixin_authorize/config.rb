b9a2701c697e1995fd105800225c82bc66c04356
lanrion <huaitao-deng@foxmail.com>
2015-06-23 17:03:19 +0800

    添加 ssl not verified 错误参考解决方案

--- lib/weixin_authorize/config.rb
+++ lib/weixin_authorize/config.rb
@@ -15,6 +15,8 @@ module WeixinAuthorize
 
     # 可选配置: RestClient timeout, etc.
     # key 必须是符号
+    # 如果出现 RestClient::SSLCertificateNotVerified Exception: SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed
+    # 这个错，除了改 verify_ssl: true，请参考：http://www.extendi.it/blog/2015/5/23/47-sslv3-read-server-certificate-b-certificate-verify-failed
     def rest_client_options
       if config.nil?
         return {timeout: 5, open_timeout: 5, verify_ssl: true}
